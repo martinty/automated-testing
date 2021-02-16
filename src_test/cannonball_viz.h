e2c96f99274e38898e273efc7f329c461c391282
Martin Tysseland <martinty@stud.ntnu.no>
2021-02-16 19:59:59 +0100

    Remove src_zip and src_test

--- src_test/cannonball_viz.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void cannonBallViz(double targetPosition, int fieldLength, double velocityX,
-                   double velocityY, int timeSteps);
--- src_test/utilities.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-
-int randomWithLimits(int min, int max);
-
-void testDeviation(double compareOperand, double toOperand, double maxError,
-                   string name);
